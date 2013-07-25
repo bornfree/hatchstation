bd12cb9e9195f7578fc8bd1e1f0cb9c4bca2573e
bornfree <harsha.xg@gmail.com>
2013-07-25 17:01:09 +0530

    Redo gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,6 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
 set :application, "hatchstation"
 set :repository, "https://github.com/bornfree/hatchstation.git"
 set :domain,  '50.116.21.162'# '72.14.191.93' #,  # '173.255.195.108'
@@ -10,7 +13,9 @@ set :user, "root"
 role :web, domain # Your HTTP server, Apache/etc
 role :app, domain # This may be the same as your `Web` server
 role :db, db_domain, :primary => true # This is where Rails migrations will run
-
+set :rvm_type, :system
+set :rvm_ruby_string, "ruby 1.9.3p392@global"
+set :rvm_path, "/usr/local/rvm/"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
